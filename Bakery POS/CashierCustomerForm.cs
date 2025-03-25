b28f74c82932f92200e479560cba58fa51feb4da
abdullah <sattiabdullah666532@gmail.com>
2025-03-25 12:00:13 +0500

    abudllah commit

--- Bakery POS/CashierCustomerForm.cs
+++ Bakery POS/CashierCustomerForm.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Windows.Forms;// just for fun
 
 namespace Bakery_POS
 {
